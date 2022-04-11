d909c535e41a9e54b17099a02718c4fe18b59c38
graysenanson <graysen.anson@gmail.com>
2022-04-11 14:59:36 -0600

    Added return to else statement.

--- src/components/CardContainer/cardContainer.tsx
+++ src/components/CardContainer/cardContainer.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { CategoryHeader } from "../CategoryHeader/categoryHeader";
 import { CardList } from "../CardList/cardList";
 
-let num = 0;
 let cNum = 0;
 const categoryObj: any = [];
 
 export const CardContainer = (props: any) =>  
   props.item.map((inv: any) => {
     //Sort by Order in json - ascending, change b - a for decending.
-    const sorted = props.item.sort(function(a: any, b: any){
+    props.item.sort(function(a: any, b: any){
       return a.Order - b.Order;
     });
 
@@ -19,7 +18,7 @@ export const CardContainer = (props: any) =>
     }
 
     //Set Category to Header component
-    //Set first cardList object, as it would bypass first object in first render.
+    //Set first cardList object, otherwise it would bypass first card object in first render.
     if (categoryObj[cNum] === inv.Category) {
       cNum++;
       return (
@@ -28,12 +27,12 @@ export const CardContainer = (props: any) =>
           <CardList key={inv.Arr_ID} items={inv} />
         </div>
       );
-    }
-    
+    } else {
     //Second render of object will paste the next values
     return (
       <div key={inv.beer_name}>
         <CardList key={inv.order} items={inv} />
       </div>
     );
+    }
   });
